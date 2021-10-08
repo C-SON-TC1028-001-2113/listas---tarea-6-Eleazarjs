d24a25cc870e65c6b30bb999aea4cdbc27a8e9c9
Eleazarjs <A01252485@itesm.mx>
2021-10-08 04:13:20 +0000

    09SerieFibonacciListas

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,6 +1,22 @@
 def main():
-    
-    pass
+    lista = [0,1]
+    m = []
+    m1 = [0]
+    a=-1
+    while a<0:
+        a = int(input(''))
+        if a==0:
+            print(m)
+        elif a>0:
+            if a==1:
+                print(m1)
+            elif a==2:
+                print(lista)
+            elif a>2:
+                for x in range(a-2):
+                    lista.append(lista[x]+lista[x+1])
+                print(lista)
+
 
 if __name__=='__main__':
     main()
